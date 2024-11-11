f77161396e1a09f384e5ca4754044b93fa8b1e32
oladotun <oladotunadekomaya@gmail.com>
2024-11-11 08:53:09 +0100

    commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let id = 5;
+console.log('ID:', id);
+// to watch a typescript file, use; 'tsc --watch <filename>' in the terminal
+// to watch all typescript files, use; 'tsc --watch' in the terminal
+// to setup a configutation file, use; 'tsc --init' int the terminal
+// The command 'tsc <filename>' compiles a typescript file to javascript
+// The command 'tsc' compiles all typescript file to javascript, and to run a typescript file,
+// it first has to be compiled to a javascript file. 
